5bd833400bea5a36f9e11e7c503897de1002d0cc
Ibrakov Azat <azatibrakov@gmail.com>
2018-05-05 11:49:03 +0300

    Add 'python_requires'

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(name='montemplate',
       author_email='azatibrakov@gmail.com',
       url=project_base_url,
       download_url=project_base_url + 'archive/master.zip',
+      python_requires='>=3.5',
       setup_requires=setup_requires,
       install_requires=install_requires,
       tests_require=tests_require)
