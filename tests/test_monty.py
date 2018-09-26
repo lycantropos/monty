016cce1f03eeffc7fc34cc51683d2fd3ef88e1b1
Ibrakov Azat <azatibrakov@gmail.com>
2018-09-26 07:10:41 +0300

    Import module instead of contents

--- tests/test_monty.py
+++ tests/test_monty.py
@@ -6,37 +6,36 @@ from typing import (Any,
 import click
 import pytest
 
-from scripts.monty import (files_paths,
-                           main)
+from scripts import monty
 
 
-def test_monty(settings_path: str,
-               template_dir: str,
-               output_dir: str,
-               github_access_token: Optional[str]) -> None:
-    monty = partial(main.callback,
-                    version=False,
-                    settings_path=settings_path,
-                    template_dir=template_dir,
-                    output_dir=output_dir,
-                    github_access_token=github_access_token)
+def test_command(settings_path: str,
+                 template_dir: str,
+                 output_dir: str,
+                 github_access_token: Optional[str]) -> None:
+    command = partial(monty.main.callback,
+                      version=False,
+                      settings_path=settings_path,
+                      template_dir=template_dir,
+                      output_dir=output_dir,
+                      github_access_token=github_access_token)
 
-    template_dir_files_count = capacity(files_paths(template_dir))
-    files_count_before = capacity(files_paths(output_dir))
+    template_dir_files_count = capacity(monty.files_paths(template_dir))
+    files_count_before = capacity(monty.files_paths(output_dir))
 
-    monty(overwrite=False)
+    command(overwrite=False)
 
-    files_count_after = capacity(files_paths(output_dir))
+    files_count_after = capacity(monty.files_paths(output_dir))
 
-    monty(overwrite=True)
+    command(overwrite=True)
 
-    files_count_after_overwrite = capacity(files_paths(output_dir))
+    files_count_after_overwrite = capacity(monty.files_paths(output_dir))
 
     assert files_count_after == files_count_before + template_dir_files_count
     assert files_count_after_overwrite == files_count_after
 
     with pytest.raises(click.BadOptionUsage):
-        monty(overwrite=False)
+        command(overwrite=False)
 
 
 def capacity(elements: Iterable[Any]) -> int:
