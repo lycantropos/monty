4235ef604e08db1308d4172b1d8df4a2cbbd4e81
Ibrakov Azat <azatibrakov@gmail.com>
2018-09-27 18:51:48 +0300

    Use 'hypothesis' strategy for email adresses

--- tests/strategies/monty.py
+++ tests/strategies/monty.py
@@ -1,13 +1,9 @@
-import re
-
 from hypothesis import strategies
 
 from .common import ascii_alphanumeric
 from .services import (dockerhub_logins,
                        github_logins)
 
-email_addresses_regex = re.compile(r'\A[^@]+@[^@]+\.[^@]+\Z')
-email_addresses = strategies.from_regex(email_addresses_regex)
 projects_names_delimiters = '.-_'
 projects_names_alphabet = strategies.sampled_from(ascii_alphanumeric
                                                   + projects_names_delimiters)
@@ -26,7 +22,7 @@ projects_names = (strategies.text(alphabet=projects_names_alphabet,
                                   min_size=2,
                                   max_size=100)
                   .filter(project_name_valid))
-settings = strategies.fixed_dictionaries({'email': email_addresses,
+settings = strategies.fixed_dictionaries({'email': strategies.emails(),
                                           'dockerhub_login': dockerhub_logins,
                                           'github_login': github_logins,
                                           'project': projects_names})
