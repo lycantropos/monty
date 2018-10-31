31cb89da516294913a511d73f22c2e051cfaf4cb
Ibrakov Azat <azatibrakov@gmail.com>
2018-10-31 20:48:33 +0300

    Bump version: 0.0.3-alpha → 0.0.3

--- monty/monty.py
+++ monty/monty.py
@@ -22,7 +22,7 @@ from strictyaml import (Map,
                         Str,
                         load)
 
-__version__ = '0.0.3-alpha'
+__version__ = '0.0.3'
 
 TRANSLATION_TABLE = bytes({7, 8, 9, 10, 12, 13, 27}
                           | set(range(0x20, 0x100))
