d12069616a29aa018a031c8c3dc1a659b4a56712
Ibrakov Azat <azatibrakov@gmail.com>
2019-01-29 12:45:57 +0300

    Complete 'conftest' modules

--- template/tests/conftest.py
+++ template/tests/conftest.py
@@ -63,11 +63,5 @@ def pytest_generate_tests(metafunc: Metafunc) -> None:
     if metafunc.config.option.repeat is None:
         return
     count = int(metafunc.config.option.repeat)
-    # We're going to duplicate these tests by parametrisation,
-    # which requires that each test has a fixture to accept the parameter.
-    # We can add a new fixture like so:
-    metafunc.fixturenames.append('tmp_ct')
-    # Now we parametrize. This is what happens when we do e.g.,
-    # @pytest.mark.parametrize('tmp_ct', range(count))
-    # def test_foo(): pass
-    metafunc.parametrize('tmp_ct', range(count))
+    metafunc.fixturenames.append('_')
+    metafunc.parametrize('_', range(count))
