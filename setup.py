727c6f428ad82b38a6a5fd3929abf6e2f40e7a90
Azat Ibrakov <azatibrakov@gmail.com>
2019-06-26 01:36:04 +0300

    Bump version: 0.1.2 → 0.2.0-alpha

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ entry_points = {
 }
 
 setup(name='montemplate',
-      version='0.1.2',
+      version='0.2.0-alpha',
       description='Python project generator.',
       long_description=Path('README.md').read_text(encoding='utf-8'),
       long_description_content_type='text/markdown',
