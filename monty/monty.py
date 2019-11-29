2dde0dc7c791e6144e9aaacd63d65981ccaad3b1
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-29 17:17:00 +0300

    Bump version: 0.2.1 → 0.3.0-alpha

--- monty/monty.py
+++ monty/monty.py
@@ -26,7 +26,7 @@ from strictyaml import (Map,
                         load)
 from strictyaml.yamllocation import YAMLChunk
 
-__version__ = '0.2.1'
+__version__ = '0.3.0-alpha'
 
 TRANSLATION_TABLE = bytes({7, 8, 9, 10, 12, 13, 27}
                           | set(range(0x20, 0x100))
