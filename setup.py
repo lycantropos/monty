f8866df11538865df9d2eae6858f8b3622e53c97
Azat Ibrakov <azatibrakov@gmail.com>
2020-03-12 15:25:57 +0300

    Bump version: 0.3.1 → 0.3.2-alpha

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 project_base_url = 'https://github.com/lycantropos/monty/'
 
 setup(name='montemplate',
-      version='0.3.1',
+      version='0.3.2-alpha',
       description='Python project generator.',
       long_description=Path('README.md').read_text(encoding='utf-8'),
       long_description_content_type='text/markdown',
